eed2ab150e55f0bb4ac6fa677f367623cddb5999
skobraf <skobraf@gmail.com>
2019-10-25 16:08:56 +0100

    added the form

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import List from './List';
 
 const App = () => (
+    <>
     <div>
-        <h2>Articles</h2>
-        <List />
+      <h2>Articles</h2>
+      <List />
     </div>
+    <div>
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
+  </>
 );
 export default App;
